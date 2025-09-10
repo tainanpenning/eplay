cc38267c3087d2753b5979345d801acbffb8cb3a
tainanpenning <tainanhartwigpenning@gmail.com>
2025-09-10 10:17:41 -0300

    Construindo o cabeçalho

---
+++ src/styles.ts
@@ -0,0 +1,31 @@
+import styled, { createGlobalStyle } from 'styled-components'
+
+export const Colors = {
+  white: '#eeeeee',
+  black: '#111111',
+  gray: '#333333',
+  green: '#10ac84'
+}
+
+export const GlobalCss = createGlobalStyle`
+  *{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: Roboto, sans-serif;
+    list-style: none;
+    text-decoration: none;
+  }
+
+  body{
+    background-color: ${Colors.black};
+    color: ${Colors.white};
+    padding-top: 40px;
+  }
+`
+
+export const Container = styled.div`
+  max-width: 1024px;
+  width: 100%;
+  margin: 0 auto;
+`
