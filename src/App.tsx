9f9d0a6820994a85fcc4350a3cd31534a0ca4a60
tainanpenning <tainanhartwigpenning@gmail.com>
2025-10-13 10:01:50 -0300

    configuração inicial da store com react-redux

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,24 @@
 import { BrowserRouter } from 'react-router-dom'
+import { Provider } from 'react-redux'
 
 import { Header } from './components/Header'
 import { GlobalCss } from './styles'
 import { RoutesManager } from './routes'
 import { Footer } from './components/Footer'
+import { store } from './store'
 
 function App() {
   return (
-    <BrowserRouter>
-      <GlobalCss />
-      <div className="container">
-        <Header />
-      </div>
-      <RoutesManager />
-      <Footer />
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <GlobalCss />
+        <div className="container">
+          <Header />
+        </div>
+        <RoutesManager />
+        <Footer />
+      </BrowserRouter>
+    </Provider>
   )
 }
 
