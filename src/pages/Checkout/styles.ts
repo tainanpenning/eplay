f5dbe6d2f65207d33c34756d986cb0bf37e79132
tainanpenning <tainanhartwigpenning@gmail.com>
2025-10-22 08:49:28 -0300

    construindo a seção de pagamento por cartão

--- src/pages/Checkout/styles.ts
+++ src/pages/Checkout/styles.ts
@@ -1,13 +1,24 @@
 import styled from 'styled-components'
 import { Colors } from '../../styles'
 
+type InputGroupProps = {
+  maxWidth?: string
+}
+
+type TabButtonProps = {
+  isActive: boolean
+}
+
 export const Row = styled.div`
   display: flex;
   column-gap: 24px;
+  align-items: flex-end;
 `
 
-export const InputGroup = styled.div`
+export const InputGroup = styled.div<InputGroupProps>`
   flex: auto;
+
+  max-width: ${(props) => props.maxWidth || 'auto'};
 
   label {
     font-size: 14px;
@@ -15,12 +26,36 @@ export const InputGroup = styled.div`
     display: block;
   }
 
-  input {
+  input,
+  select {
     background-color: ${Colors.white};
     border: 1px solid ${Colors.white};
     border-radius: 4px;
     height: 32px;
     width: 100%;
     padding: 0 8px;
+  }
+`
+
+export const TabButton = styled.button<TabButtonProps>`
+  border-radius: 8px;
+  font-size: 14px;
+  font-weight: bold;
+  color: ${Colors.white};
+  background-color: ${(props) =>
+    props.isActive ? Colors.green : Colors.black};
+  height: 32px;
+  border: none;
+  margin-right: 16px;
+  padding: 0 8px;
+  display: inline-flex;
+  align-items: center;
+
+  img {
+    margin-right: 8px;
+  }
+
+  &:hover {
+    cursor: pointer;
   }
 `
